a61b806f1073cc6a59057ab5d83ae4bdec9fd8f1
Chander-Shekhar <cs17btech11011@iith.ac.in>
2020-12-16 01:42:18 +0530

    Print performance stats

--- coarse-grain lock/coarse-grain-hash-table.cpp
+++ coarse-grain lock/coarse-grain-hash-table.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 class runner{
 public:
-	time_t waiting_time,waiting_time_exit;
+	vector<time_t> waiting_time;
 	runner(int n,int m,int l1){
 		N=n;
 	    M=m;
@@ -19,7 +19,7 @@ public:
 	    File_Filter = fopen("CLH-Lock.txt","w");
 	    // File.open("Hash-table.txt");
 	    // hash=new unordered_map<int,int>();
-	    waiting_time=0;
+	    waiting_time = vector<time_t>(n);
 	}
 	~runner(){
 		fclose(File_Filter);
@@ -57,7 +57,7 @@ public:
             auto end = chrono::system_clock::now();
             time_t actEnterTime=chrono::system_clock::to_time_t(end);
             auto elapsed=chrono::duration_cast<chrono::microseconds>(end - start);
-            waiting_time += elapsed.count();
+            waiting_time[id] += elapsed.count();
             // max_waiting_writers=max(max_waiting_writers,elapsed.count());
             // fprintf(File_Filter,"%dth complete in hash at %s by thread %d\n",i+1,getTimeinhr(actEnterTime).c_str(),id+1);
             // File << i+1 <<"th CS Entry at "<<getTimeinhr(reqEnterTime)<<" by thread "<<id+1<<" (mesg 2)"<<endl;
@@ -83,21 +83,33 @@ int main()
 {
 	// ifstream input;
 	// input.open("inp-params.txt");//get inputs from file
-	int N=128,M=5000,L1=5;
-	// input>>N>>M>>L1;
-	thread th[N];
-	runner * runPtr= new runner(N,M,L1);
-	cout << N << endl;
-	for(int i=0;i<N;i++)
+	int NM = 1048576;
+	for( int N=2;N<=512;N=N*2)
 	{
-		th[i]=thread(&runner::proc, runPtr,i);
+		int M=NM/N,L1=5;
+		// input>>N>>M>>L1;
+		thread th[N];
+		runner * runPtr= new runner(N,M,L1);
+		auto start = chrono::system_clock::now();
+		for(int i=0;i<N;i++)
+		{
+			th[i]=thread(&runner::proc, runPtr,i);
+		}
+		for(int i = 0; i < N; ++i)
+		{
+			th[i].join();
+		}
+		auto end = chrono::system_clock::now();
+		time_t tot_time=0;
+		for(int i=0;i<N;i++)
+		{
+			tot_time+=runPtr->waiting_time[i];
+		}
+		auto elapsed=chrono::duration_cast<chrono::microseconds>(end - start);
+		printf("Total wait time to enter for Hash-table with %d threads is %ld microseconds\n",N,tot_time);
+		printf("elapsed is %ld microseconds\n",elapsed.count());
+		// printf("Average wait time to enter for Hash-table is %ld microseconds\n",runPtr->waiting_time/(N*M));
+		// printf("Average wait time to exit for MCS lock is %ld\n",runPtr->waiting_time_exit/(N*M));
 	}
-	for(int i = 0; i < N; ++i)
-	{
-		th[i].join();
-	}
-	
-    printf("Average wait time to enter for Hash-table is %ld microseconds\n",runPtr->waiting_time/(N*M));
-    // printf("Average wait time to exit for MCS lock is %ld\n",runPtr->waiting_time_exit/(N*M));
 	return 0;
 }
